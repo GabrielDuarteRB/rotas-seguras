f36538b919a8285d1d9252e8c85f41e2ce8693af
GabrielDuarteRB <gabriel.drbastos@gmail.com>
2025-05-14 13:45:22 -0300

    feat: Policial viatura rotas

--- app/src/policial/policial.module.ts
+++ app/src/policial/policial.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { PolicialViatura } from './entities/policial-viatura.entity';
 import { PolicialService } from './policial.service';
 import { PolicialController } from './policial.controller';
+import { PolicialViaturaController } from './policial-viatura.controller';
+import { PolicialRepository } from './policial.repository';
 
 @Module({
-  controllers: [PolicialController],
-  providers: [PolicialService],
+  imports : [SequelizeModule.forFeature([PolicialViatura])],
+  controllers: [PolicialController, PolicialViaturaController],
+  providers: [PolicialService, PolicialRepository],
 })
 export class PolicialModule {}
